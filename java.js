bbe17f510a9d11f2da4ffd0729a74c5350209873
Cat-Hanbit <dreamnawas@naver.com>
2021-05-03 17:08:15 +0900

    시작 인터페이스 + 영화관 이름 리턴

---
+++ java.js
@@ -0,0 +1,37 @@
+let theather;
+
+
+let button = document.querySelector("#CGV_1");
+button.addEventListener("click", ()=>
+{
+    let theather = "http://www.cgv.co.kr/theaters/?theaterCode=0049";
+    console.log(theather);
+})
+
+let button2 = document.querySelector("#CGV_2");
+button2.addEventListener("click", ()=>
+{
+    let theather = "http://www.cgv.co.kr/theaters/?theaterCode=0012";
+    console.log(theather);
+})
+
+let button3 = document.querySelector("#CGV_3");
+button3.addEventListener("click", ()=>
+{
+    let theather = "http://www.cgv.co.kr/theaters/?theaterCode=0041";
+    console.log(theather);
+})
+
+let button4 = document.querySelector("#CGV_4");
+button4.addEventListener("click", ()=>
+{
+    let theather = "http://www.cgv.co.kr/theaters/?theaterCode=0242";
+    console.log(theather);
+})
+
+let button5 = document.querySelector("#CGV_5");
+button5.addEventListener("click", ()=>
+{
+    let theather = "http://www.cgv.co.kr/theaters/?theaterCode=0257";
+    console.log(theather);
+})
